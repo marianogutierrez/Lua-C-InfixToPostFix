a0912a86c0539f03537189f9e6bd6298361d5827
Mariano Gutierrezls <margutierrez75@gmail.com>
2018-04-15 10:34:32 -0700

    prog3

---
+++ prog3_3.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+extern "C" {
+#include "lua.h"
+#include "lualib.h"
+#include "luaxlib.h"
+}
+
+
+int main(int argc, char* argv[]) {
+char buff[256]; // allocated buffers
+char tolua[256];
+lua_State *L = luaL_newstate();
+luaL_openlibs(L);
+luaL_dofile(L,argv[1]);
+// now retrieve from stdin
+while(1 = 1) {
+fgets(buff,256,stdin);
+buff[strnlen(buff)-1] =0; // null it cause appends newline
+sprintf(tolua,"return InfixToPostfix:(\"%s\")"",buff); // * auto terminated with null
+luaL_dostring(L,buff); // Lua please take this string and execute it
+}
+
+char *output = luaL_checkstring(L,1); // 1 mean first arguement to whatever was being called
+printf("%s\n", output);
+lua_close(L); // always do this
+return 0;
+
+}
