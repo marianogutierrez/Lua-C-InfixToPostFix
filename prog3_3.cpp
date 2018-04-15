fb5641db8d0fbdb28623f56e69d53cf10738497d
Mariano Gutierrezls <margutierrez75@gmail.com>
2018-04-15 11:17:12 -0700

    prog3

--- prog3_3.cpp
+++ prog3_3.cpp
@@ -13,16 +13,15 @@ lua_State *L = luaL_newstate();
 luaL_openlibs(L);
 luaL_dofile(L,argv[1]);
 // now retrieve from stdin
-while(1 = 1) {
+
 fgets(buff,256,stdin);
-buff[strnlen(buff)-1] =0; // null it cause appends newline
-sprintf(tolua,"return InfixToPostfix:(\"%s\")"",buff); // * auto terminated with null
+buff[strnlen(buff)-1] = 0; // null it cause appends newline
+sprintf(tolua,"return InfixToPostfix:(\"%s\")"", buff); // * auto terminated with null
 luaL_dostring(L,buff); // Lua please take this string and execute it
-}
+
 
 char *output = luaL_checkstring(L,1); // 1 mean first arguement to whatever was being called
 printf("%s\n", output);
 lua_close(L); // always do this
 return 0;
-
 }
