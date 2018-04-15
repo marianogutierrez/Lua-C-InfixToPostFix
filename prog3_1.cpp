43dd7f56e0f5bf13ac3d540857eb03e51cee4eac
Mariano Gutierrezls <margutierrez75@gmail.com>
2018-04-14 17:41:41 -0700

    prog3

--- prog3_1.cpp
+++ prog3_1.cpp
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include "lua.h" // connect
 #include "lualib.h"
-#include <stdlib.h>
-#include <iostream>
 #include "luaxlib.h" // lua auxiallry  NOTE: L AUX
 // C is the host and thus defines everything
 // g++ prog3_1.cpp – o prog3_1 – I lua - 5.3.4/src – L lua - 5.3.4/src – l lua – l m – l dl
@@ -21,6 +19,6 @@ int main(int argc, char *argv[]) {
      // allocate lua stack on the heap auxilalary lib
    luaL_openlibs(L); // puts things inside box for Lua to use
    luaL_dofile(L,argv[1]);
-   luaL_close(L); // always do this
+   lua_close(L); // always do this
     return 0;
   }
