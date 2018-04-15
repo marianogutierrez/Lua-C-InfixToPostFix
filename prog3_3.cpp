d52ea41091da3c844a68cf7d45ee768d484f7ee5
Mariano Gutierrezls <margutierrez75@gmail.com>
2018-04-15 11:39:32 -0700

    prog3

--- prog3_3.cpp
+++ prog3_3.cpp
@@ -17,8 +17,8 @@ luaL_dofile(L,argv[1]);
 
 fgets(buff,256,stdin);
 buff[strnlen(buff)-1] = "\0"; // null it cause appends newline
-sprintf(tolua,"return InfixToPostfix(\"%s\")"", buff); // * auto terminated with null
-luaL_dostring(L,tolua); // Lua please take this string and execute it
+sprintf(tolua,"return InfixToPostfix(\"%s\")", buff); // * auto terminated with null
+luaL_dostring(L,buff); // Lua please take this string and execute it
 
 
 const char *output = luaL_checkstring(L,1); // 1 mean first arguement to whatever was being called
