0b2cac929a0185a97dd783e35363fe9ee74b8e2d
Mariano Gutierrezls <margutierrez75@gmail.com>
2018-04-14 23:26:12 -0700

    prog3

--- prog3_2.lua
+++ prog3_2.lua
@@ -1,5 +1,3 @@
-
-
 object = {}
 function object:new(t)
   local obj = t or {}
@@ -9,31 +7,25 @@ function object:new(t)
 end
 
 -- globals
-stack = {
-    tok = {}
-    len = 0 -- arrays however begin at one 
-}
-queue = {
-    len = 0
-    data = {}
-    front = 1
-} 
+stack = { tok = {} ,len = 0 }
+queue = { len = 0, data = {}, front = 1 }
 
 stk = object:new(stack)
 q = object:new(queue)
 
 ----- STACK
-function stack:push(x) -- 
-    self.len = len + 1
-    self.tok[len] = x
+function stack:push(x) --
+    self.len = self.len + 1
+    self.tok[self.len] = x
 end
 
 function stack:pop()
     if(self.len == 0) then
     print("No toks")
+    return
 end
     local mytok = self.tok[self.len]
-   table.remove(self.tok,len)  -- no need for return type apparently.
+   table.remove(self.tok,self.len)  -- no need for return type apparently.
     self.len = self.len -1
     return mytok
 end
@@ -42,13 +34,15 @@ function stack:peek()
 
   if(self.len == 0) then
     print("No toks")
+    return
 end
     return self.tok[self.len] -- LIFO
 end
 ------- QUEUE
-function queue:enqueue(x) 
+function queue:enqueue(x)
+      self.len = self.len + 1
      self.data[self.len] = x
-     len =len + 1
+    -- self.len = self.len + 1
 end
 
 
@@ -56,39 +50,42 @@ function queue:dequeue()
 if(self.len == 0) then
     return nil
 end
-    local mytok = self.tok[front]
-    table.remove(self.data,front); 
+    local mytok = self.data[self.front]
+    table.remove(self.data,self.front);
     self.len = self.len - 1
     return mytok
 
 end
 
 -- lower or equal precedence
-function precedence(input,comp) {
-    if(input == "(" or ")" and comp ==  "(" or ")"  then
+function precedence(input,comp)
+    if(input == "(" or input == ")" and comp ==  "(" or comp == ")")  then
         return true -- against itself
-    else 
+    else
         return false; -- higher always
-
-if (input == "*" or "/") then    
+    end
+if (input == "*" or "/") then
         if(comp == "(" or ")" or "*" or "/") then
             return true -- lower or equal
         else
             return false
+          end
 elseif (input == "+" or "-") then
     if(comp ==  "(" or ")" or "*" or "/" or "+" or "-") then
     return true
        else return false
+       end
 else
-   return false -- nil 
-
+   return false -- nil
+end
+end
 
 function InfixToPostfix(str)
-    
+
 -- Tokenizes the input string by space, apply algo, and return postfix string
     local arg = str
-for tok in arg: gmatch("%S") do -- splits it by the space 
-    if( tok not( "(" or ")" or "*" or "/" or "+" or "-"))
+for tok in arg: gmatch("%S") do -- splits it by the space
+    if(tok ~= "(" and tok ~=  ")" and tok ~=  "*" and tok ~=  "/" and tok ~=  "+" and tok ~=  "-")
         then -- must be a number
         q:enqueue(tok)
     else
@@ -97,20 +94,20 @@ for tok in arg: gmatch("%S") do -- splits it by the space
             stk:push(tok)
             end
     stk:push(tok)
-end -- for / while not empty 
+end
+end
 -- finally
-while(stk.len not 0) 
+while(stk.len ~= 0)
     do
         q:enqueue(stk:pop())
 end
 
 local out = {}
- while(q.len not 0)
+ while(q.len ~= 0)
     do
         table.insert(out,q:dequeue())
 end
 
 return out
-end -- end fucntion
-
-
+ -- thought this was the for loop
+ end
