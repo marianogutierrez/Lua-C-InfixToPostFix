a2e69516ba48239b5d6a66779eed158d531f9d3c
Mariano Gutierrez <margutierrez75@gmail.com>
2018-04-15 19:42:43 +0000

    Update prog3_3.cpp

--- prog3_3.cpp
+++ prog3_3.cpp
@@ -9,7 +9,7 @@ extern "C" {
 
 
 int main(int argc, char* argv[]) {
-printf("Assignment #3-1, Mariano Gutierrez, margutierrez75@gmail.com\n");
+printf("Assignment #3-3, Mariano Gutierrez, margutierrez75@gmail.com\n");
 char buff[256]; // allocated buffers
 char tolua[256];
 lua_State *L = luaL_newstate();
