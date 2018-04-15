df1af1bc881aa1c4cb37dd33b4308d5a8238434b
Mariano Gutierrezls <margutierrez75@gmail.com>
2018-04-14 17:19:08 -0700

    prog3

--- prog3_1.cpp
+++ prog3_1.cpp
@@ -5,13 +5,13 @@
 // C is the host and thus defines everything
 // g++ prog3_1.cpp – o prog3_1 – I lua - 5.3.4/src – L lua - 5.3.4/src – l lua – l m – l dl
 
-using namespace std;
+
 /*This program will take in a single command line arg
 which will be a Lua file and then execute it. */
 int main(int argc, char *argv[]) {
   if(argc != 2) { // program counts as an arguement NOTE: starts at zero
-    cout << endl;
-    cout << "This program expects a single command line argument." << endl;
+    std::cout << endl;
+    std::cout << "This program expects a single command line argument." << std::endl;
     return 1; // end it; the user broke my stuff
   }
 
