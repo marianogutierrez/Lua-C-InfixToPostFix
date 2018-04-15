a81af02c921c9a2dc009e79ceb94242116f1b443
Mariano Gutierrezls <margutierrez75@gmail.com>
2018-04-14 17:33:22 -0700

    prog3

--- prog3_1.cpp
+++ prog3_1.cpp
@@ -3,7 +3,7 @@
 #include "lualib.h"
 #include <stdlib.h>
 #include <iostream>
-//#include "luaxlib.h" // lua auxiallry  NOTE: L AUX
+#include "luaxlib.h" // lua auxiallry  NOTE: L AUX
 // C is the host and thus defines everything
 // g++ prog3_1.cpp – o prog3_1 – I lua - 5.3.4/src – L lua - 5.3.4/src – l lua – l m – l dl
 
@@ -17,10 +17,10 @@ int main(int argc, char *argv[]) {
     return 1; // end it; the user broke my stuff
   }
 
-    lua_State *L = luaL_newstate(); // newState makes compiler mad, bc cant link
+    lua::lua_State *L = luaL_newstate(); // newState makes compiler mad, bc cant link
      // allocate lua stack on the heap auxilalary lib
-    luaL_openlibs(L); // puts things inside box for Lua to use
-    luaL_dofile(L,argv[1]);
-    luaL_close(L); // always do this
+  lua::  luaL_openlibs(L); // puts things inside box for Lua to use
+  lua::  luaL_dofile(L,argv[1]);
+  lua::  luaL_close(L); // always do this
     return 0;
   }
